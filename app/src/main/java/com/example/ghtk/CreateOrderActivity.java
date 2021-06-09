028c162cd1e6044d8946b81d76df372fa98da665
nenechiii <khoi2809@gmail.com>
2021-06-09 09:40:15 +0700

    change some Button to TextView in CreateOrderActivity

--- app/src/main/java/com/example/ghtk/CreateOrderActivity.java
+++ app/src/main/java/com/example/ghtk/CreateOrderActivity.java
@@ -17,12 +17,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CreateOrderActivity extends AppCompatActivity {
     ImageButton iBBack;
     CheckBox checkBox4;
-    Button bGoods, bCreate;
+    Button bCreate;
+    TextView bGoods, bCOD, bServiceChoose, bPhotoDelAll, bAddGoods;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +32,13 @@ public class CreateOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_order);
 
         checkBox4 = findViewById(R.id.checkBox4);
-        bGoods = findViewById(R.id.b_goods);
         bCreate = findViewById(R.id.b_create);
+        bGoods = findViewById(R.id.b_goods);
+        bCOD = findViewById(R.id.b_cod);
+        bServiceChoose = findViewById(R.id.b_service_choose);
+        bPhotoDelAll = findViewById(R.id.b_photo_delete_all);
+        bAddGoods = findViewById(R.id.add_goods);
 
-        bGoods.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
-            }
-        });
 
         bCreate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,8 +50,14 @@ public class CreateOrderActivity extends AppCompatActivity {
         });
 
         String text = "Tôi đã đọc và đồng ý với Điều khoản và quy định";
+        String text2 = "Xem thêm quy định";
+        String text3 = "Quy định thanh toán COD";
+        String text4 = "Chọn dịch vụ";
+        String text5 = "Xoá tất cả";
+        String text6 = "Thêm hàng hoá";
+
         SpannableString ss = new SpannableString(text);
-        ClickableSpan clickableSpan = new ClickableSpan() {
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
             @Override
             public void onClick(@NonNull View widget) {
                 startActivity(new Intent(CreateOrderActivity.this, ContactActivity.class));
@@ -64,11 +70,100 @@ public class CreateOrderActivity extends AppCompatActivity {
                 ds.setUnderlineText(false);
             }
         };
-
-        ss.setSpan(clickableSpan, 25,47, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ss.setSpan(clickableSpan1, 25,47, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         checkBox4.setText(ss);
         checkBox4.setMovementMethod(LinkMovementMethod.getInstance());
         checkBox4.setHighlightColor(ContextCompat.getColor(this, R.color.color9F5A7B));
+
+        ss = new SpannableString(text2);
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(70, 168, 167));
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan2, 0,17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        bGoods.setText(ss);
+        bGoods.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ss = new SpannableString(text3);
+        ClickableSpan clickableSpan3 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(159, 90, 123));
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan3, 0,23, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        bCOD.setText(ss);
+        bCOD.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ss = new SpannableString(text4);
+        ClickableSpan clickableSpan4 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(70, 168, 167));
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan4, 0,12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        bServiceChoose.setText(ss);
+        bServiceChoose.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ss = new SpannableString(text5);
+        ClickableSpan clickableSpan5 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(70, 168, 167));
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan5, 0,10, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        bPhotoDelAll.setText(ss);
+        bPhotoDelAll.setMovementMethod(LinkMovementMethod.getInstance());
+
+        ss = new SpannableString(text6);
+        ClickableSpan clickableSpan6 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(CreateOrderActivity.this, RegulationActivity.class));
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(159, 90, 123));
+                ds.setUnderlineText(false);
+            }
+        };
+        ss.setSpan(clickableSpan6, 0,13, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        bAddGoods.setText(ss);
+        bAddGoods.setMovementMethod(LinkMovementMethod.getInstance());
 
     }
 }
